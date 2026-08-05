332d7b6b3dee6331d7a38f649d681fcafb8bd3d9
Abdihakim Mohamed <abdihakin18@gmail.com>
2026-08-05 14:45:26 +0300

    feat(certificate-management-service): initialize project structure and docker configuration

---
+++ certificate-management-service/src/test/java/so/cb/pki/CertificateManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package so.cb.pki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
