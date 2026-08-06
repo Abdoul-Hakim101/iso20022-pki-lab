2b6a0f1d2da33594982f452073e5ca35abe24676
Abdihakim Mohamed <abdihakin18@gmail.com>
2026-08-06 10:50:29 +0300

    feat(adapter): initialize bank-payment-adapter with organized pom.xml, BouncyCastle, and Apache Santuario XMLSec

---
+++ bank-payment-adapter/src/main/java/so/cb/adapter/BankPaymentAdapterApplication.java
@@ -0,0 +1,13 @@
+package so.cb.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankPaymentAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankPaymentAdapterApplication.class, args);
+	}
+
+}
---
+++ bank-payment-adapter/src/test/java/so/cb/adapter/BankPaymentAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package so.cb.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankPaymentAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
